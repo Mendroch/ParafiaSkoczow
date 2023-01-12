452892c3b8ac862b99cdb9384239a2126a466378
OPTIMAL IT <optimalit@Mac-mini-OPTIMAL.local>
2023-01-12 09:44:31 +0100

    Initial-commit

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#111111',
+  },
+  fontSize: {
+    xl: '24px',
+    l: '20px',
+    m: '16px',
+    s: '12px',
+  },
+};
---
+++ src/views/Root.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "assets/styles/GlobalStyle";
+import { theme } from "assets/styles/theme";
+
+const Root = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
+
+export default Root;
